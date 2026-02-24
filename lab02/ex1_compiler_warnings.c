d4424f1af6f8003c6552b106426c9a70d1d09571
limit-coding <2024210195@bupt.cn>
2026-02-24 11:13:15 +0800

    loading lab02

--- lab02/ex1_compiler_warnings.c
+++ lab02/ex1_compiler_warnings.c
@@ -10,9 +10,11 @@ struct Course {
 struct Course *make_course(int id, char *name) {
     struct Course *new_course = malloc(sizeof(struct Course));
     new_course->id = id;
-    new_course->name = *name;
-
-    return &new_course;
+    new_course->name = name;
+    //这里需要修改，不然成野指针了
+    new_course->name=strdup(name);
+    //需要返回对象地址，而非局部变量地址
+    return new_course;
 }
 int main() {
     struct Course *cs161 = make_course(161, "Computer Security");
